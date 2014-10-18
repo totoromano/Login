e39a307e12daa3643e7eb50f0752258cbd0c44c2
cromanor <dev@claudios-imac.attlocal.net>
2014-10-17 21:25:57 -0400

    Login Repo Initial Commit

---
+++ Login/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Login
+//
+//  Created by Dev on 10/17/14.
+//  Copyright (c) 2014 Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Login/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Login
+//
+//  Created by Dev on 10/17/14.
+//  Copyright (c) 2014 Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
