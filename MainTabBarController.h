10c41555d807e6d3e7108382c56be7cba689fb4b
cromanor <dev@claudios-imac.attlocal.net>
2014-10-25 16:48:12 -0400

    Added MainController for Tabs

---
+++ MainTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  MainTabBarController.h
+//  Login
+//
+//  Created by Dev on 10/25/14.
+//  Copyright (c) 2014 Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTabBarController : UITabBarController
+
+@end
---
+++ MainTabBarController.m
@@ -0,0 +1,38 @@
+//
+//  MainTabBarController.m
+//  Login
+//
+//  Created by Dev on 10/25/14.
+//  Copyright (c) 2014 Dev. All rights reserved.
+//
+
+#import "MainTabBarController.h"
+
+@interface MainTabBarController ()
+
+@end
+
+@implementation MainTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.tabBar.barTintColor = [UIColor colorWithRed:44/255.0 green:51/255.0 blue:52/255.0 alpha:0.95];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
