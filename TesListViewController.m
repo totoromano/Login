867028c06087e714abc21bd10ed106fd2e3cd559
cromanor <dev@claudios-imac.attlocal.net>
2014-11-09 20:49:54 -0500

    Changed the font for search box

--- TesListViewController.m
+++ TesListViewController.m
@@ -54,6 +54,7 @@
     [self.view setBackgroundColor:[UIColor colorWithRed:44/255.0 green:51/255.0 blue:52/255.0 alpha:0.95]];
     self.searchBox.textColor = [UIColor whiteColor];
     self.searchBox.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Search" attributes:@{NSForegroundColorAttributeName: [UIColor whiteColor]}];
+    self.searchBox.font = [UIFont fontWithName:@"HelveticaNeue-Thin" size:17.0f];
     [self.tableView setBackgroundColor:[UIColor clearColor]];
     [self.tableView setDelegate:self];
     [self.tableView setDataSource:self];
